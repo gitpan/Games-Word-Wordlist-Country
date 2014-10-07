e8049602f6a672b84f5539bdd5781e1c43f0d0ce
perlancar <perlancar@gmail.com>
2014-10-07 15:09:49 +0000

    Import of PERLANCAR/Games-Word-Wordlist-Country-0.01 from CPAN.
    gitpan-cpan-distribution: Games-Word-Wordlist-Country
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PERLANCAR/Games-Word-Wordlist-Country-0.01.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

---
+++ lib/Games/Word/Wordlist/Country.pm
@@ -0,0 +1,81 @@
+package Games::Word::Wordlist::Country;
+
+our $DATE = '2014-10-07'; # DATE
+our $VERSION = '0.01'; # VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use parent qw(Games::Word::Wordlist);
+
+our $COUNTRIES = ["afghanistan","aland islands","albania","algeria","american samoa","andorra","angola","anguilla","antarctica","antigua and barbuda","argentina","armenia","aruba","australia","austria","azerbaijan","bahamas","bahrain","bangladesh","barbados","belarus","belgium","belize","benin","bermuda","bhutan","bolivia","bonaire, sint eustatius and saba","bosnia and herzegovina","botswana","bouvet island","brazil","british indian ocean territory","brunei","bulgaria","burkina faso","burma","burundi","cabo verde","cambodia","cameroon","canada","cayman islands","central african republic","chad","chile","china","christmas island","cocos islands","colombia","comoros","congo","congo-kinshasa","cook islands","costa rica","cote d'ivoire","croatia","cuba","curacao","cyprus","czech republic","denmark","djibouti","dominica","dominican republic","east timor","ecuador","egypt","el salvador","equatorial guinea","eritrea","estonia","ethiopia","falkland islands (malvinas)","faroe islands","federated states of micronesia","fiji","finland","france","french guiana","french polynesia","french southern territories","gabon","gambia","georgia","germany","ghana","gibraltar","greece","greenland","grenada","guadeloupe","guam","guatemala","guernsey","guinea","guinea-bissau","guyana","haiti","heard island and mcdonald islands","holy see","honduras","hong kong","hungary","iceland","india","indonesia","iran","iraq","ireland","isle of man","israel","italy","jamaica","japan","jersey","jordan","kazakstan","kenya","kiribati","kuwait","kyrgyzstan","lao people's democratic republic","latvia","lebanon","lesotho","liberia","libya","liechtenstein","lithuania","luxembourg","macao","macedonia","madagascar","malawi","malaysia","maldives","mali","malta","marshall islands","martinique","mauritania","mauritius","mayotte","mexico","monaco","mongolia","montenegro","montserrat","morocco","mozambique","namibia","nauru","nepal","netherlands","new caledonia","new zealand","nicaragua","niger","nigeria","niue","norfolk island","north korea","northern mariana islands","norway","oman","pakistan","palau","panama","papua new guinea","paraguay","peru","philippines","pitcairn","poland","portugal","puerto rico","qatar","republic of moldova","reunion","romania","russian federation","rwanda","saint barthelemy","saint helena","saint kitts and nevis","saint lucia","saint martin","saint pierre and miquelon","saint vincent and the grenadines","samoa","san marino","sao tome and principe","saudi arabia","senegal","serbia","seychelles","sierra leone","singapore","sint maarten (dutch part)","slovakia","slovenia","solomon islands","somalia","south africa","south georgia and the islands","south korea","south sudan","spain","sri lanka","state of palestine","sudan","suriname","svalbard and jan mayen","swaziland","sweden","switzerland","syria","taiwan","tajikistan","thailand","togo","tokelau","tonga","trinidad and tobago","tunisia","turkey","turkmenistan","turks and caicos islands","tuvalu","uganda","uk","ukraine","united arab emirates","united republic of tanzania","united states minor outlying islands","uruguay","us","uzbekistan","vanuatu","venezuela, bolivarian republic of ","vietnam","virgin islands","virgin islands (uk)","wallis and futuna","western sahara","yemen","zambia","zimbabwe"]; # PRECOMPUTE
+
+sub new {
+    bless Games::Word::Wordlist->new ($COUNTRIES), shift;
+}
+
+1;
+
+# ABSTRACT: English names of countries of the world
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Games::Word::Wordlist::Country - English names of countries of the world
+
+=head1 VERSION
+
+This document describes version 0.01 of Games::Word::Wordlist::Country (from Perl distribution Games-Word-Wordlist-Country), released on 2014-10-07.
+
+=head1 SYNOPSIS
+
+  use Games::Word::Wordlist::Country;
+  my $wl = Games::Word::Wordlist::Country->new;
+  my $word = $wl->random_word;
+  print "We have a word." if $wl->is_word ($word);
+
+=head1 DESCRIPTION
+
+This module provides English names of countries of the world. You can use this
+in some word games, e.g. L<Games::Word::Hangman>.
+
+The list is produced using the latest L<Locale::Codes> during build time, using
+the shortest name for each country, then converted to lowercase. Some words will
+contain whitespace (e.g. "Sri Lanka") or parentheses (e.g. "Virgin Islands
+(UK)"). If you only want single words, you can use
+L<Games::Word::Wordlist::CountrySingleWord>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Games-Word-Wordlist-Country>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/perlancar/perl-Games-Word-Wordlist-Country>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Games-Word-Wordlist-Country>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+perlancar <perlancar@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by perlancar@cpan.org.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ lib/Games/Word/Wordlist/CountrySingleWord.pm
@@ -0,0 +1,71 @@
+package Games::Word::Wordlist::CountrySingleWord;
+
+our $DATE = '2014-10-07'; # DATE
+our $VERSION = '0.01'; # VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use parent qw(Games::Word::Wordlist);
+
+require Games::Word::Wordlist::Country;
+
+our $COUNTRIES = [grep {/\A\w+\z/} @$Games::Word::Wordlist::Country::COUNTRIES];
+
+sub new {
+    bless Games::Word::Wordlist->new ($COUNTRIES), shift;
+}
+
+1;
+
+# ABSTRACT: English names of countries of the world (single words only)
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Games::Word::Wordlist::CountrySingleWord - English names of countries of the world (single words only)
+
+=head1 VERSION
+
+This document describes version 0.01 of Games::Word::Wordlist::CountrySingleWord (from Perl distribution Games-Word-Wordlist-Country), released on 2014-10-07.
+
+=head1 SYNOPSIS
+
+=head2 SEE ALSO
+
+See L<Games::Word::Wordlist::Country>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Games-Word-Wordlist-Country>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/perlancar/perl-Games-Word-Wordlist-Country>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Games-Word-Wordlist-Country>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+perlancar <perlancar@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by perlancar@cpan.org.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
